4909477407862715db0e7f7dc0d1e78893b2f4d9
jeongyun-kim <kkanyo04@naver.com>
2024-09-16 15:31:21 +0900

    Chore: 환경설정 및 기본 필요 리소스 정의

---
+++ PopupLog/Resources/Resources.swift
@@ -0,0 +1,34 @@
+//
+//  Resources.swift
+//  PopupLog
+//
+//  Created by 김정윤 on 9/16/24.
+//
+
+import SwiftUI
+import UIKit
+
+enum Resources {
+    enum Images {
+        static let setting = "gearshape.fill"
+        static let search = "magnifyingglass"
+        static let plus = "plus"
+        static let chart = "chart.bar.fill"
+        static let menu = "line.3.horizontal"
+    }
+    
+    enum Colors {
+        static let primaryColor = Color(hex: 0xFF8911)
+        static let lightOrange = Color(hex: 0xfcecc7)
+        static let primaryColorUK = UIColor(red: 1.00, green: 0.54, blue: 0.07, alpha: 1.00)
+        static let white: Color = .white
+        static let black: Color = .black
+        static let systemGray6: Color = Color(.systemGray6)
+        static let lightGray: Color = Color(.lightGray)
+    }
+    
+    enum Fonts {
+        static let bold15 = UIFont.systemFont(ofSize: 15, weight: .bold)
+        static let regular14 = UIFont.systemFont(ofSize: 14)
+    }
+}
