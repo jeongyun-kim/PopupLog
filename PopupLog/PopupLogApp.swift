b239ebf3fa48e13b8eea850fe416839a9886b4a6
jeongyun-kim <kkanyo04@naver.com>
2024-09-16 15:13:44 +0900

    Initial Commit

---
+++ PopupLog/PopupLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupLogApp.swift
+//  PopupLog
+//
+//  Created by 김정윤 on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PopupLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
