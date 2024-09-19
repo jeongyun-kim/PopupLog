f2acc9541dc07f42d389c9d43584e2e0988b71f5
jeongyun-kim <kkanyo04@naver.com>
2024-09-20 01:39:12 +0900

    Feat: 태그 모델 생성 및 기본 태그 리스트 구성

---
+++ PopupLog/Database/Model/Tag.swift
@@ -0,0 +1,36 @@
+//
+//  Tag.swift
+//  PopupLog
+//
+//  Created by 김정윤 on 9/19/24.
+//
+
+import SwiftUI
+import RealmSwift
+
+final class Tag: Object, ObjectKeyIdentifiable {
+    @Persisted (primaryKey: true) var id: ObjectId
+    @Persisted var emoji: String
+    @Persisted var tagName: String
+    @Persisted var tagColor: String?
+    
+    convenience init(emoji: String, tagName: String, tagColor: String?) {
+        self.init()
+        self.emoji = emoji
+        self.tagName = tagName
+        self.tagColor = tagColor
+    }
+}
+
+struct DefaultTags {
+    static let defaultTagList = [
+        Tag(emoji: "😊", tagName: "기타", tagColor: Resources.Colors.systemGray6.toHex()),
+        Tag(emoji: "💖", tagName: "캐릭터", tagColor: "F3CCF3"),
+        Tag(emoji: "💄", tagName: "뷰티", tagColor: "FED9ED"),
+        Tag(emoji: "🧢", tagName: "의류", tagColor: "FBF0B2"),
+        Tag(emoji: "🍰", tagName: "디저트", tagColor: "FFF4EA"),
+        Tag(emoji: "📚", tagName: "문구", tagColor: "D5ED9F"),
+        Tag(emoji: "💻", tagName: "전자기기", tagColor: "CAEDFF")
+    ]
+}
+
