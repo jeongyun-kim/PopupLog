5cedc32297272e59d95ff3c25a8ac3091f95f406
jeongyun-kim <kkanyo04@naver.com>
2024-09-24 17:08:03 +0900

    Refactor: ImageOnChangeWrapper 코드 수정
    - Image를 결과로 내보내던 것 UIImage로 변경
    - 중복 코드 개선

--- PopupLog/Resources/Wrapper/ImageOnChangeWrapper.swift
+++ PopupLog/Resources/Wrapper/ImageOnChangeWrapper.swift
@@ -11,39 +11,45 @@ import PhotosUI
 // 사용자가 이미지를 선택하였을 때, 변경된 이미지 반환
 struct ImageOnChangeWrapper: ViewModifier {
     @Binding var pickerItem: PhotosPickerItem?
-    let action: (Image) -> Void
+    let action: (UIImage) -> Void
     
     func body(content: Content) -> some View {
         if #available(iOS 17.0, *) {
             content
                 .onChange(of: pickerItem) {
-                    guard let pickerItem else { return }
-                    Task {
-                        if let image = try? await pickerItem.loadTransferable(type: Image.self) {
-                            action(image)
-                        } else {
-                            print("Failed")
-                        }
+                    getUIImage(pickerItem) { image in
+                        action(image)
                     }
                 }
         } else {
             content
                 .onChange(of: pickerItem, perform: { _ in
-                    guard let pickerItem else { return }
-                    Task {
-                        if let image = try? await pickerItem.loadTransferable(type: Image.self) {
-                            action(image)
-                        } else {
-                            print("Failed")
-                        }
+                    getUIImage(pickerItem) { image in
+                        action(image)
                     }
                 })
+        }
+    }
+    
+    private func getUIImage(_ pickerItem: PhotosPickerItem?, completionHandler: @escaping (UIImage) -> Void) {
+        guard let pickerItem else { return }
+        Task {
+            if let imageData = try? await pickerItem.loadTransferable(type: Data.self) {
+                if let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        completionHandler(image)
+                    }
+                }
+            }
+            else {
+                print("Failed Get UIImage")
+            }
         }
     }
 }
 
 extension View {
-    func changedImage(_ item: Binding<PhotosPickerItem?>, action: @escaping (Image) -> Void) -> some View {
+    func changedImage(_ item: Binding<PhotosPickerItem?>, action: @escaping (UIImage) -> Void) -> some View {
         modifier(ImageOnChangeWrapper(pickerItem: item, action: action))
     }
 }
