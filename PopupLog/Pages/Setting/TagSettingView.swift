37625eb8e446d471803d1b58391f4dc224182fd2
jeongyun-kim <kkanyo04@naver.com>
2024-09-28 00:44:59 +0900

    Refactor: AddOrEditTagView 화면전환 Navigation -> Sheet로 변경 / 불필요한 코드 제거 및 코드 정리

--- PopupLog/Pages/Setting/TagSettingView.swift
+++ PopupLog/Pages/Setting/TagSettingView.swift
@@ -9,89 +9,28 @@ import SwiftUI
 import RealmSwift
 
 struct TagSettingView: View {
-    @EnvironmentObject var viewStatus: CalendarViewStatus
+    @EnvironmentObject var viewStatus: CalendarViewStatus // 캘린더뷰 내 항목들
     @ObservedResults (Tag.self) private var tagList
-    @State private var color = Color.red
+    @State private var isPresenting = false // AddOrEditTagView 보여지고 있는지
+    @State private var selectedTag = Tag() // 다음 뷰로 넘겨줄 태그
     
     var body: some View {
         ScrollView(showsIndicators: false, content: {
             VStack(alignment: .leading) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: Resources.Radius.textContents)
-                        .fill(Resources.Colors.white)
-                    NavigationLink {
-                        LazyNavigationView(AddOrEditTagView())
-                    } label: {
-                        HStack {
-                            Text("태그 생성")
-                                .foregroundStyle(Resources.Colors.black)
-                                .padding()
-                            Spacer()
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                }
-                .padding()
+                tagAddRowView()
                 
                 Text("기본 태그 목록")
                     .font(.caption)
                     .foregroundStyle(Resources.Colors.lightGray)
                     .padding(.horizontal, 24)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: Resources.Radius.textContents)
-                        .fill(Resources.Colors.white)
-                    LazyVStack(alignment: .leading) {
-                        ForEach(tagList.filter { $0.isDefault }, id: \.id) { tag in
-                            HStack {
-                                TagButton(tag: tag, action: {})
-                                    .disabled(true)
-                                Spacer()
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding(4)
-                        }
-                    }
-                    .padding(8)
-                }
-                .padding(.horizontal)
+                defaultTagListView()
                 
                 Text("사용자 태그 목록")
                     .font(.caption)
                     .foregroundStyle(Resources.Colors.lightGray)
                     .padding(.horizontal, 24)
                     .padding(.top)
-                ZStack {
-                    RoundedRectangle(cornerRadius: Resources.Radius.textContents)
-                        .fill(Resources.Colors.white)
-                        .overlay {
-                            if tagList.filter({ !$0.isDefault }).isEmpty {
-                                RoundedRectangle(cornerRadius: Resources.Radius.textContents)
-                                    .fill(Resources.Colors.moreLightOrange)
-                            }
-                        }
-                    LazyVStack {
-                        ForEach(tagList.filter { !$0.isDefault }, id: \.id) { tag in
-                            NavigationLink {
-                                LazyNavigationView(AddOrEditTagView(tag: tag))
-                            } label: {
-                                HStack {
-                                    TagButton(tag: tag, action: {})
-                                        .disabled(true)
-                                    Spacer()
-                                    Resources.Images.next
-                                        .foregroundStyle(Resources.Colors.lightGray)
-                                }
-                                .padding(4)
-                            }
-                            .frame(maxWidth: .infinity)
-                        }
-                    }
-                    .padding(8)
-                }
-                .padding(.horizontal)
-                
-                
+                nonDefaultTagListView()
             }
         })
         .background(Resources.Colors.moreLightOrange)
@@ -105,6 +44,96 @@ struct TagSettingView: View {
         .onDisappear {
             viewStatus.isPresentingBottomSheet = true
         }
-        
+        .sheet(isPresented: $isPresenting, content: {
+            SheetTagView(tag: $selectedTag, isPresenting: $isPresenting)
+        })
+    }
+}
+
+struct SheetTagView: View {
+    @Binding var tag: Tag
+    @Binding var isPresenting: Bool
+    
+    var body: some View {
+        LazyNavigationView(AddOrEditTagView(tag: tag))
+    }
+}
+
+// MARK: ViewUI
+extension TagSettingView {
+    // MARK: 태그 생성
+    private func tagAddRowView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: Resources.Radius.textContents)
+                .fill(Resources.Colors.white)
+            Button {
+                selectedTag = Tag()
+                isPresenting.toggle()
+            } label: {
+                HStack {
+                    Text("태그 생성")
+                        .foregroundStyle(Resources.Colors.black)
+                        .padding()
+                    Spacer()
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .padding()
+    }
+    
+    // MARK: 기본 태그 리스트
+    private func defaultTagListView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: Resources.Radius.textContents)
+                .fill(Resources.Colors.white)
+            LazyVStack(alignment: .leading) {
+                ForEach(tagList.filter { $0.isDefault }, id: \.id) { tag in
+                    HStack {
+                        TagButton(tag: tag, action: {})
+                            .disabled(true)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(4)
+                }
+            }
+            .padding(8)
+        }
+        .padding(.horizontal)
+    }
+    
+    // MARK: 사용자 태그 리스트
+    private func nonDefaultTagListView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: Resources.Radius.textContents)
+                .fill(Resources.Colors.white)
+                .overlay {
+                    if tagList.filter({ !$0.isDefault }).isEmpty {
+                        RoundedRectangle(cornerRadius: Resources.Radius.textContents)
+                            .fill(Resources.Colors.moreLightOrange)
+                    }
+                }
+            LazyVStack {
+                ForEach(tagList.filter { !$0.isDefault }, id: \.id) { tag in
+                    Button {
+                        selectedTag = tag
+                        isPresenting.toggle()
+                    } label: {
+                        HStack {
+                            TagButton(tag: tag, action: {})
+                                .disabled(true)
+                            Spacer()
+                            Resources.Images.next
+                                .foregroundStyle(Resources.Colors.lightGray)
+                        }
+                        .padding(4)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(8)
+        }
+        .padding(.horizontal)
     }
 }
