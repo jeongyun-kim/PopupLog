89fee717a783b542bbc9c855ce6aa08d8fdcb76b
jeongyun-kim <kkanyo04@naver.com>
2025-11-10 10:59:00 +0900

    [Chore] 위젯 추가위한 설정

---
+++ CalendarWidget/CalendarWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarWidgetBundle.swift
+//  CalendarWidget
+//
+//  Created by 김정윤 on 11/10/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CalendarWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CalendarWidget()
+    }
+}
