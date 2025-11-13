e9704bafdb87952980c77015e47e20dea5420d4f
jeongyun-kim <kkanyo04@naver.com>
2025-11-13 14:49:02 +0900

    [Chore] 버전 변경

--- PopupLog/Pages/Calendar/SideMenu/MenuContentsView.swift
+++ PopupLog/Pages/Calendar/SideMenu/MenuContentsView.swift
@@ -121,7 +121,7 @@ extension MenuContentsView {
                     .font(.callout)
                     .foregroundStyle(Resources.Colors.lightGray)
             })
-            Text("버전 1.4.1")
+            Text("버전 1.4.2")
                 .font(.caption)
                 .foregroundStyle(Resources.Colors.lightGray)
         }
